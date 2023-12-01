873e9485cfa29f7b6a6749d4904b4bd124037e0b
bagasahmad <90127013+bagasahmad@users.noreply.github.com>
2023-12-01 14:30:58 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,23 +1,45 @@
-const { sendPhotoHandler, getAllDataMissingPerHandle } = require('./handler');
+const { sendPhotoHandler, getAllDataMissingPerHandle, tambahDataOrangHilangHandler, editDataOrangHilangHandler, hapusDataOrangHilangHandler, dataOrangHilang } = require("./handler");
 
 const routes = [
-    {
-        method: 'GET',
-        path: '/find',
-        handler: getAllDataMissingPerHandle
+  {
+    method: "GET",
+    path: "/find",
+    handler: getAllDataMissingPerHandle,
+  },
+  {
+    method: "POST",
+    path: "/find",
+    config: {
+      payload: {
+        output: "stream",
+        parse: true,
+        multipart: true,
+      },
     },
-    {
-        method: 'POST',
-        path: '/find',
-        config: {
-            payload: {
-                output: 'stream',
-                parse: true,
-                multipart: true,
-            }
-        },
-        handler: sendPhotoHandler,
+    handler: sendPhotoHandler,
+  },
+  {
+    method: "POST",
+    path: "/tambah-orang-hilang",
+    handler: tambahDataOrangHilangHandler, // Rute untuk menambah data orang hilang
+  },
+  {
+    method: "PUT",
+    path: "/edit-orang-hilang/{id}",
+    handler: editDataOrangHilangHandler, // Rute untuk mengedit data orang hilang
+  },
+  {
+    method: "DELETE",
+    path: "/hapus-orang-hilang/{id}",
+    handler: hapusDataOrangHilangHandler, // Rute untuk menghapus data orang hilang
+  },
+  {
+    method: "GET",
+    path: "/data-orang-hilang",
+    handler: (request, h) => {
+      return h.response({ data: dataOrangHilang }).code(200); // Rute untuk mendapatkan data orang hilang
     },
-]
+  },
+];
 
 module.exports = routes;
