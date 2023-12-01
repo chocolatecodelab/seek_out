895365218d032d47e56ea0292a075a6417a8e9f9
Muhammad Nazar Gunawan <alialfatih303@gmail.com>
2023-12-01 10:53:35 +0800

    membuat koneksi firestore dan api endpoint sebagian

--- routes.js
+++ routes.js
@@ -1,5 +1,23 @@
+const { sendPhotoHandler, getPhotoHandler } = require('./handler');
+
 const routes = [
-    
+    {
+        method: 'GET',
+        path: '/find',
+        handler: getPhotoHandler
+    },
+    {
+        method: 'POST',
+        path: '/find',
+        config: {
+            payload: {
+                output: 'stream',
+                parse: true,
+                multipart: true,
+            }
+        },
+        handler: sendPhotoHandler,
+    },
 ]
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -2,16 +2,33 @@
 
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const Inert = require('@hapi/inert')
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+          },
     });
 
     server.route(routes);
+
+    await server.register(Inert);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
